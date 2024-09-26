980f012502628e85ba1870e78dc8f17144bcf2bf
zioan <zaharia.ioan@gmail.com>
2024-09-26 17:50:46 +0200

    Refactoring ErrorBoundary and api handler to use error context

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import authService from "../lib/authService";
-import { HttpError } from "../util/ErrorBoundary";
+import { HttpError } from "../context/ErrorContext";
 
 const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
--- src/util/ErrorBoundary.js
+++ src/util/ErrorBoundary.js
@@ -1,25 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { toast } from "react-toastify";
-
-export const handleErrorRedirect = (error) => {
-  if (!error) return null;
-
-  switch (error.status) {
-    case 400:
-      return <Navigate to="/error" state={{ error }} />;
-    case 401:
-      return <Navigate to="/unauthorized" state={{ error }} />;
-    case 403:
-      return <Navigate to="/forbidden" state={{ error }} />;
-    case 404:
-      return <Navigate to="/not-found" state={{ error }} />;
-    case 500:
-      return <Navigate to="/server-error" state={{ error }} />;
-    default:
-      return <Navigate to="/error" state={{ error }} />;
-  }
-};
+import { HttpError } from "../context/ErrorContext";
 
 export class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -33,28 +14,11 @@ export class ErrorBoundary extends React.Component {
 
   render() {
     if (this.state.hasError) {
-      return handleErrorRedirect(this.state.error);
+      if (this.state.error instanceof HttpError) {
+        return <Navigate to={this.state.error.getRedirectPath()} state={{ error: this.state.error }} />;
+      }
+      return <Navigate to="/error" state={{ error: this.state.error }} />;
     }
     return this.props.children;
   }
 }
-
-export class HttpError extends Error {
-  constructor(message, status) {
-    super(message);
-    this.name = "HttpError";
-    this.status = status;
-  }
-}
-
-export const handleApiError = (err, fallbackMessage) => {
-  const errorMessage = err.response?.data?.message || fallbackMessage;
-  const statusCode = err.response?.status || 500;
-  if (!(err instanceof HttpError)) {
-    toast.error(errorMessage, {
-      toastId: `error-${statusCode}`, // Prevent duplicate toasts
-    });
-    throw new HttpError(errorMessage, statusCode);
-  }
-  throw err;
-};
