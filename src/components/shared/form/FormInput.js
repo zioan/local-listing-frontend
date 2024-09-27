f92993697903f2e2aff11e5266455e908e80e8fc
zioan <zaharia.ioan@gmail.com>
2024-09-27 13:08:06 +0200

    Prevent default browser autocompletion for input fields

--- src/components/shared/form/FormInput.js
+++ src/components/shared/form/FormInput.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const FormInput = ({ id, name, value, onChange, label, type = "text", required = false }) => {
+const FormInput = ({ id, name, value, onChange, label, type = "text", required = false, autoComplete = "off" }) => {
   return (
     <div>
       <label htmlFor={id} className="block text-sm font-medium leading-6 text-gray-900">
@@ -13,6 +13,7 @@ const FormInput = ({ id, name, value, onChange, label, type = "text", required =
         value={value}
         onChange={onChange}
         required={required}
+        autoComplete={autoComplete}
         className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
       />
     </div>
